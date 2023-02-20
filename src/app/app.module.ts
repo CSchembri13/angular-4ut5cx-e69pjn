93004f573a1de627cb9c9635b2787035fa8afe57
CSchembri13 <73115719+CSchembri13@users.noreply.github.com>
2023-02-20 13:34:02 +0100

    4 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
@@ -11,21 +12,13 @@ import { ProductListComponent } from './product-list/product-list.component';
   imports: [
     BrowserModule,
     ReactiveFormsModule,
-    RouterModule.forRoot([
-      { path: '', component: ProductListComponent },
-    ])
+    FormsModule,
+    RouterModule.forRoot([{ path: '', component: ProductListComponent }]),
   ],
-  declarations: [
-    AppComponent,
-    TopBarComponent,
-    ProductListComponent
-  ],
-  bootstrap: [
-    AppComponent
-  ]
+  declarations: [AppComponent, TopBarComponent, ProductListComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
-
+export class AppModule {}
 
 /*
 Copyright Google LLC. All Rights Reserved.
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { ToolbarModule } from 'primeng/toolbar';
 import { PanelModule } from 'primeng/panel';
-import { Product,products } from '../products';
+import { Product, products } from '../products';
 
 @Component({
   selector: 'app-product-list',
@@ -9,10 +9,23 @@ import { Product,products } from '../products';
   styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent {
-  products :Product[] = products;
+  products: Product[] = products;
+
+  object_to_add = { id: 1, name: '', price: 1, description: '' };
 
   share() {
     window.alert('The product has been shared!');
+  }
+
+  onSubmit(data: any) {
+    this.object_to_add = {
+      id: products.length + 1,
+      name: data.name,
+      price: data.price,
+      description: data.desc,
+    };
+    console.warn(data);
+    products.push(this.object_to_add);
   }
 }
 
